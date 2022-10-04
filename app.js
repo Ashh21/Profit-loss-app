1334b5ab8cc31b22788c47d0d21d5e05923ed90c
Ayush shrivastava <106957781+Ashh21@users.noreply.github.com>
2022-10-04 17:13:42 +0530

    error handle

--- app.js
+++ app.js
@@ -17,21 +17,23 @@ function plhandler(){
 
 function calculateProfitAndLoss(buy, current, qty){
     if (buy > current){
-       const loss = ((buy - current)* qty).toFixed(2) ;
-       const lossPercentage = ((loss/buy) *100).toFixed(2) ;
+       const loss = ((buy - current) * qty).toFixed(2) ;
+       const lossPercentage = ((loss / buy) * 100).toFixed(2) ;
 
        showOutput(`The loss is ${loss} and loss percent is ${lossPercentage} %`) ;
     } 
-     if (current > buy) {
-        const profit = ((current - buy)* qty).toFixed(2) ;
-        const profitPercentage = ((profit/buy)* 100).toFixed(2) ;
+    
+    else if (current > buy) {
+        const profit = ((current - buy) * qty).toFixed(2) ;
+        const profitPercentage = ((profit / buy) * 100).toFixed(2) ;
 
         showOutput(`The proft is ${profit} and profit percent is ${profitPercentage} %`) ;
-
     }
     else if (buy = current){
-        showOutput("No profit and no loss");
+       showOutput("No profit and no loss");
     }
+
+
     else{
         alert("Fill all the empty fields first to calculate!!")
     }
@@ -40,3 +42,5 @@ function calculateProfitAndLoss(buy, current, qty){
 function showOutput(msg){
     message.innerText = msg ;
 }
+
+
